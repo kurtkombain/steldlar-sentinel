d63bb8dac3e165efd4dd73f967b24fea0711ae52
kurtkombain <aldionerdjan6@hotmail.com>
2023-06-24 14:00:01 +0200

    Add support for ERC-721A tokens

--- blockchain_layer_3_protocols.go
+++ blockchain_layer_3_protocols.go
@@ -1,3 +1,4 @@
 // Enhance privacy features using zk-STARKs
 // Add support for multiple consensus mechanisms
 // Improve contract interaction via APIs
+// Improve error handling in transaction flow
