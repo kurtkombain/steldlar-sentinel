db8b21c25e8e381813cd29a4cd6a82196c7ec38e
kurtkombain <aldionerdjan6@hotmail.com>
2023-09-09 14:00:01 +0200

    Fix issues in token transfer logic

--- token_vesting_schedule.go
+++ token_vesting_schedule.go
@@ -1,3 +1,4 @@
 // Enhance decentralized autonomous organizations
 // Refactor governance token logic
 // Fix security vulnerability in random number generation
+// Enhance governance contract flexibility
