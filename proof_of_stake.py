62b704096d1126abaa1c3525252888e90c4ad933
kurtkombain <aldionerdjan6@hotmail.com>
2023-11-10 14:00:00 +0200

    Fix ERC-20 token interface bugs

--- proof_of_stake.py
+++ proof_of_stake.py
@@ -1,3 +1,4 @@
 // Optimize contract storage for gas reduction
 // Integrate with decentralized storage systems
 // Improve ERC-721 token metadata functionality
+// Refactor contract to use EIP-712 signatures
