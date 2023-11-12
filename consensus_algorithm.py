861a095432d78edbfd146505433c36e401bc135f
kurtkombain <aldionerdjan6@hotmail.com>
2023-11-12 14:00:00 +0200

    Refactor validator reward system

--- consensus_algorithm.py
+++ consensus_algorithm.py
@@ -1,3 +1,4 @@
 // Integrate with decentralized storage systems
 // Add multi-chain governance support
 // Optimize WASM compilation for Go contracts
+// Add support for zk-SNARKs in smart contracts
