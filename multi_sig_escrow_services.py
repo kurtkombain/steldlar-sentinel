65716ca416239b85f657dae7a831d6800b9fad88
kurtkombain <aldionerdjan6@hotmail.com>
2023-11-27 14:00:00 +0200

    Optimize token burn process

--- multi_sig_escrow_services.py
+++ multi_sig_escrow_services.py
@@ -2,3 +2,4 @@
 // Add support for multi-signature wallet contracts
 // Implement price feed oracles for tokens
 // Fix compatibility issues with major wallets
+// Improve contract verification speed
