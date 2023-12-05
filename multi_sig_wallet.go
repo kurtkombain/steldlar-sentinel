d11c1017344e5dd4450106b858a943bb78778e7a
kurtkombain <aldionerdjan6@hotmail.com>
2023-12-05 14:00:00 +0200

    Fix governance proposal approval logic

--- multi_sig_wallet.go
+++ multi_sig_wallet.go
@@ -1,3 +1,4 @@
 // Improve staking and yield farming logic
 // Optimize state channels for faster transactions
 // Fix issues with token transfer confirmations
+// Implement zero-knowledge proof (ZKP) support
