382047e13e4694790e3f0cec532a8c548e357693
kurtkombain <aldionerdjan6@hotmail.com>
2024-01-11 14:00:00 +0200

    Refactor reward pooling algorithm

--- multisignature_approval.go
+++ multisignature_approval.go
@@ -1,3 +1,4 @@
 // Add support for zk-SNARKs in smart contracts
 // Fix interoperability issues with other blockchains
 // Fix type mismatch in Haskell smart contract
+// Refactor incentive structure in DeFi protocol
