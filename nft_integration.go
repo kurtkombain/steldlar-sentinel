5836d06313cf04ded4f6eb84cc4002fdd2a4b764
kurtkombain <aldionerdjan6@hotmail.com>
2024-01-29 14:00:00 +0200

    Improve transaction batching efficiency

--- nft_integration.go
+++ nft_integration.go
@@ -1,3 +1,4 @@
 // Implement automatic transaction retries
 // Refactor governance token logic
 // Fix wallet interaction issues in DeFi protocols
+// Fix wallet interaction issues in DeFi protocols
