c081f77af523a43e7c99298da25686b369b1841e
kurtkombain <aldionerdjan6@hotmail.com>
2024-02-06 14:00:01 +0200

    Add new RPC endpoint

--- reward_distribution.go
+++ reward_distribution.go
@@ -1,3 +1,4 @@
 // Add support for decentralized lending platforms
 // Enhance governance contract flexibility
 // Refactor incentive structure in DeFi protocol
+// Enhance smart contract upgradeability
