ab91e9f61adcefe8c8e5db5a66486fd46bb49867
kurtkombain <aldionerdjan6@hotmail.com>
2024-03-29 14:00:00 +0200

    Add support for ERC-721A tokens

--- multi_chain_compatibility.rs
+++ multi_chain_compatibility.rs
@@ -1,3 +1,4 @@
 // Fix issue with multi-chain transactions
 // Improve security in decentralized applications
 // Improve security in decentralized applications
+// Upgrade to latest Solidity version
