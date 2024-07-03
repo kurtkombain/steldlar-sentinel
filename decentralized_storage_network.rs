a107f9aedb6d8d513177834457870ea1eed68927
kurtkombain <aldionerdjan6@hotmail.com>
2024-07-03 14:00:01 +0200

    Refactor incentive structure in DeFi protocol

--- decentralized_storage_network.rs
+++ decentralized_storage_network.rs
@@ -3,3 +3,4 @@
 // Implement on-chain fee adjustment mechanisms
 // Refactor decentralized governance structure
 // Refactor contract to use EIP-712 signatures
+// Add support for delegation in governance
