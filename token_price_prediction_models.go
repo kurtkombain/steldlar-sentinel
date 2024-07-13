a975195fcaddb87b84c2285e1813e22f1bc58015
kurtkombain <aldionerdjan6@hotmail.com>
2024-07-13 14:00:00 +0200

    Add fallback mechanism in Solidity contract

--- token_price_prediction_models.go
+++ token_price_prediction_models.go
@@ -1,3 +1,4 @@
 // Optimize Oracle-based data fetching
 // Improve decentralized lending contract flexibility
 // Fix issue with multi-chain transactions
+// Improve decentralized lending contract flexibility
