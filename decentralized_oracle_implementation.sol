66cccd453157ad46f5bf822a2f772b571ab89111
kurtkombain <aldionerdjan6@hotmail.com>
2024-07-13 14:00:01 +0200

    Add new RPC endpoint

--- decentralized_oracle_implementation.sol
+++ decentralized_oracle_implementation.sol
@@ -1,3 +1,4 @@
 // Fix errors in contract interaction with wallets
 // Implement zero-knowledge proof (ZKP) support
 // Improve transaction batching efficiency
+// Improve contract interaction via APIs
