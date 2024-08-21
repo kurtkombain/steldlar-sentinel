d36c114a6c5c8a1f99338ac838c764185136f4d8
kurtkombain <aldionerdjan6@hotmail.com>
2024-08-21 14:00:00 +0200

    Refactor contract to use EIP-712 signatures

--- transaction_fee_adjustment.sol
+++ transaction_fee_adjustment.sol
@@ -2,3 +2,4 @@
 // Add support for decentralized credit scoring
 // Add support for multiple consensus mechanisms
 // Fix vulnerability in decentralized finance protocols
+// Enhance privacy features using zk-STARKs
