1ea1a41b83514fc525e20d15b85293fce26867ab
kurtkombain <aldionerdjan6@hotmail.com>
2024-09-27 14:00:00 +0200

    Fix ERC-1155 contract interactions

--- peer_to_peer_networking.go
+++ peer_to_peer_networking.go
@@ -1,3 +1,4 @@
 // Refactor governance token logic
 // Add fallback mechanism in Solidity contract
 // Refactor smart contract testing framework
+// Fix security vulnerability in random number generation
