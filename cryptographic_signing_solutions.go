ea80023d56a71c4618c5203b147cdb203bcd2db8
kurtkombain <aldionerdjan6@hotmail.com>
2025-02-08 14:00:01 +0200

    Add multi-sig wallet functionality

--- cryptographic_signing_solutions.go
+++ cryptographic_signing_solutions.go
@@ -1,3 +1,4 @@
 // Fix bugs in NFT metadata handling
 // Enhance contract upgrade process
 // Refactor storage layout for better efficiency
+// Fix security vulnerability in random number generation
